f71d7a8a6e88e9853487a1769f5cb45459e986c8
Himani Sahoo <himani.sahoo@valtech.com>
2023-06-09 09:57:11 +0530

    commit 5

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -43,19 +43,21 @@ function Home(){
     
     return(
        
-        <div className='container '>
+        <div>
 
-           
-           <div className='userDiv'>
+            <header className='header d-flex justify-content-between'>
+                <h2>PROJECT</h2>
+                <p>Assignment</p>
+            </header>
+            <div className="container-fluid">
+           <div className='d-flex justify-content-center m-4'>
                 <div className="form-group mx-sm-3 mb-2">
                         <input type="text" className="form-control" onChange={(e)=>setTokenVal(e.target.value)} id="inputtext" placeholder="Token"/>
                 </div>
-                <button type="submit" className="btn btn-primary mb-2 " onClick={submitToken}>Submit</button>
+                <button type="submit" className="btn btn-primary mb-2 subbtn " onClick={submitToken}>Submit</button>
            </div>
             {showData && (
             <div>
-             
-                
                 {commitData.map((data)=>{
                     return(
 
@@ -69,80 +71,11 @@ function Home(){
                 </div>
                 )
             }
+            </div>
         </div>
     )
 }
 
 export default Home;
 
-// import axios from 'axios';
-// import React, { useState, useEffect } from 'react';
-// import './home.css';
 
-// function Home() {
-//   const [commitData, setCommitData] = useState([]);
-//   const [tokenVal, setTokenVal] = useState('');
-//   const [tokenValid, setTokenValid] = useState(true);
-
-//   useEffect(() => {
-//     if (tokenValid) {
-//       axios.get('https://api.github.com/repos/HimaniSahoo/UpdatedCommit/commits', {
-//         headers: {
-//           Authorization: `Bearer ${tokenVal}`,
-//         },
-//       })
-//         .then((response) => {
-//           setCommitData(response.data);
-//         })
-//         .catch((error) => {
-//           console.log(error);
-//         });
-//     }
-//   }, [tokenVal, tokenValid]);
-
-//   const submitToken = () => {
-
-//     axios.get('https://api.github.com/user', {
-//       headers: {
-//         Authorization: `Bearer ${tokenVal}`,
-//       },
-//     })
-//       .then(() => {
-//         setTokenValid(true);
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//         setTokenValid(false);
-//       });
-//   };
-
-//   return (
-//     <div className="container">
-//       <div className="form-group mx-sm-3 mb-2">
-//         <input
-//           type="text"
-//           className="form-control"
-//           onChange={(e) => setTokenVal(e.target.value)}
-//           id="inputtext"
-//           placeholder="Token"
-//         />
-//       </div>
-//       <button type="submit" className="btn btn-primary mb-2" onClick={submitToken}>
-//         Submit
-//       </button>
-//       <div>
-//         {!tokenValid && <p>Invalid token. Please enter a valid token.</p>}
-//         {commitData.map((data) => (
-//           <div key={data.id} className="userCommitDivs">
-//             <p>{data.commit.message}</p>
-//             <p>
-//               Committed by {data.commit.author.name} on {data.commit.author.date}
-//             </p>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Home;
